a67f8ef89c2ec986560c2449ce8ce950dd2ffdc4
Galford92 <115945030+Galford92@users.noreply.github.com>
2022-11-10 22:29:00 -0500

    Add files via upload

---
+++ PRueba IBM.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **John Alexander Sanchez Cardozo**
+# 
+# Currently I'm working as a teacher and as researcher in a University in Colombia. However I'm interested in learning Data Science to find a new job.
+
+# *I'm pretty interested in ML models and, I want to improve my coding skills, that's why I want to learn Data Science and then apply to a new job or even my own entrepreneurship*
+
+# ### The following cell will receive a name, stored as and object and then print it
+
+# In[3]:
+
+
+My_name = "John Alexander Sánchez Cardozo"
+print(My_name)
+
+
+# ---
+# * line 1
+# * line 2
+# * line 3
+# 
+# 1. line 4
+# 2. line 3
+# 3. line 2
+# 4. line 1
